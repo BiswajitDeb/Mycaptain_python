7740a6e1b145ea3d7ba89ce8d85c58baa5cd601c
BiswajitDeb <biswa2015.bd@gmail.com>
2021-04-14 18:42:47 +0530

    Task 2 update

--- Task 2/positive numbers in range.py
+++ Task 2/positive numbers in range.py
@@ -1,4 +1,14 @@
-n=int(input())
+l = []
+k=[]
+n= int(input("Enter the number of elements you want to input : "))
 for i in range(n):
-    if i>0:
-        print(i)
+    X=int(input())
+    l.append(X)
+for j in l:
+    if j>0:
+        k.append(j)
+
+    #if j <0:
+    #   del l[l.index(j)]
+
+print(k)
