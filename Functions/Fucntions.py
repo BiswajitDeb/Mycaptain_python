cc105cba962e0a58e8a88a4be2273e8ba48a28bc
BiswajitDeb <biswa2015.bd@gmail.com>
2021-04-24 22:22:07 +0530

    Task 3

---
+++ Functions/Fucntions.py
@@ -0,0 +1,23 @@
+s=input("Please enter a string: ")
+
+freq={}
+
+for char in s:
+    if char in freq:
+        freq[char]+=1
+    else:
+        freq[char]=1
+lst=[]
+k=0
+for i in freq:
+    k=freq[i]
+    lst.append(k)
+   
+lst.sort()
+lst.reverse()
+
+for i in lst:
+    for j in freq:
+        if(i==freq[j]):
+            print(j," = ",i)
+            freq[j]=0
